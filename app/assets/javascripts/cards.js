e9280b8d430498e21dbe7091458a70146c047882
Josh Hadik <josh.hadik@gmail.com>
2019-01-22 16:15:15 -0600

    hotfix: close card before redirecting to card link

--- app/assets/javascripts/cards.js
+++ app/assets/javascripts/cards.js
@@ -1,18 +1,42 @@
-$(document).on('turbolinks:load page:change', () => {
-  // Scroll corresponding Card View in from bottom when ShowCard action is activated.
-  ActionHelper.getElementByAction('showCard').on('click', (e) => {
-    e.preventDefault();
-    var $target = $(e.currentTarget);
-    var cardTag = $target.data("cardTag")
-    var $cardCanvas = $(`[data-card-canvas='${cardTag}']`);
+const Card = {
+  show(cardButton) {
+    const $cardButton = $(cardButton);
+    const cardTag = $cardButton.data("cardTag")
+    const $cardCanvas = $(`[data-card-canvas='${cardTag}']`);
 
     $("body").prepend("<div class='cover'></div>")
     $(".cover").animate({"opacity": "0.5"}, 300)
 
-    $cardCanvas.css("top", '100vh');
+    $cardCanvas.css("top", "100vh");
     $cardCanvas.toggleClass("active");
     $cardCanvas.animate({"top": "0"}, 300, "swing")
-  })
+  },
+
+  hide(callback) {
+    const $cardCanvas = $('.card-canvas.active');
+
+    $cardCanvas.animate({"top": "100vh"}, 300, "swing", () => {
+      $cardCanvas.removeClass("active");
+    });
+
+    $cardCanvas.promise().done(() => {
+      if(typeof callback !== 'undefined'){
+        callback();
+      };
+    })
+
+    $(".cover").animate({"opacity": "0"}, 300, () => {
+      $(".cover").remove();
+    });
+  }
+}
+
+$(document).on('turbolinks:load page:change', () => {
+  // Scroll corresponding Card View in from bottom when ShowCard action is activated.
+  ActionHelper.getElementByAction('showCard').on('click', (e) => {
+    e.preventDefault();
+    Card.show(e.currentTarget)
+  });
 
   // Add underline to close text when hovering over any area outside of the open card view.
   $(".card-canvas").on('mouseover', (e) => {
@@ -26,18 +50,18 @@ $(document).on('turbolinks:load page:change', () => {
     e.stopPropagation();
   })
 
+  // Close card before redirecting to link
+  $(".card a").on('click', (e) => {
+    e.preventDefault();
+    Card.hide(() => {
+      window.location = $(e.currentTarget).attr("href");
+    })
+  })
+
   // Close card view when surrounding area of close button are clicked.
   $(".card-canvas").on('click', (e) => {
-    const $target = $(e.currentTarget)
-
-    if ($(e.target).is($target) || $(e.target).hasClass("close")) {
-      $target.animate({"top": "100vh"}, 300, "swing", function() {
-        $target.removeClass("active");
-      })
-
-      $(".cover").animate({"opacity": "0"}, 300, function() {
-        $(".cover").remove();
-      })
+    if ($(e.target).is(e.currentTarget) || $(e.target).hasClass("close")) {
+      Card.hide()
     }
   })
 })
